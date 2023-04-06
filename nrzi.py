8c08bb7ce52fbb453231ca0b092afd1c25f6a948
Ivan <83954673+zkhrv@users.noreply.github.com>
2023-04-06 14:49:42 +0300

    Add files via upload

--- nrzi.py
+++ nrzi.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker 
 
 def string_to_bits(s):
     bits = []
@@ -12,20 +13,29 @@ def bits_to_string(b):
         chars.append(int(b[i:i+8], 2))
     return bytes(chars).decode('cp1251')
 
-def plot_encoded_signal(encoded_signal):
-    x = []
-    y = []
-    current_level = 0
-    for bit in encoded_signal:
-        if bit == '1':
-            current_level = 1 - current_level
-        x.append(len(x))
-        y.append(current_level)
-    plt.plot(x, y, color='red')
-    plt.title('NRZI Encoding')
-    plt.xlabel('Bit')
-    plt.ylabel('Signal level')
-    plt.show()
+def plot_encoded_signal(encoded_signal): 
+    x = [] 
+    y = [] 
+    current_level = 0 
+    for bit in encoded_signal: 
+        if bit == '1': 
+            current_level = 1 - current_level 
+        x.append(len(x)) 
+        y.append(current_level) 
+    plt.plot(x, y, color='red', linewidth = 3)
+    plt.xlabel(ticker.MultipleLocator(1)) #
+    plt.grid(which='major', 
+             color = 'k',
+             linewidth = 2) 
+    plt.minorticks_on() 
+    plt.grid(which='minor', 
+        color = 'gray', 
+        linestyle = '-',
+        linewidth = 1) 
+    plt.title('NRZI Encoding') 
+    plt.xlabel('Bit') 
+    plt.ylabel('Signal level') 
+    plt.show() 
 
 def nrzi_encode_decode():
     input_string = input("Введите строку для кодирования: ")
